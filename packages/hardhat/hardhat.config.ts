abb7e91059ec1abec19c05153ebfc04bdd417d19
adairrr <adair.kelley@vanderbilt.edu>
2021-03-03 19:26:20 -0600

    Add missing things to hardhat config, probably broke everything anyway.

--- packages/hardhat/hardhat.config.ts
+++ packages/hardhat/hardhat.config.ts
@@ -12,8 +12,10 @@ import '@openzeppelin/hardhat-upgrades';
 import "hardhat-gas-reporter";
 import "hardhat-typechain";
 import "solidity-coverage";
-import "@typechain/ethers-v5"
+import "@typechain/ethers-v5";
+import "@tenderly/hardhat-tenderly";
 
+import "@nomiclabs/hardhat-etherscan";
 
 // ts modified from https://github.com/magnet-finance/magnet-ui/blob/cbbc3fb6d999c2f9a5638bcc65c3a8792072d4bd/packages/hardhat/hardhat.config.ts
 
@@ -155,6 +157,11 @@ const config: HardhatUserConfig = {
   },
   gasReporter: {
     enabled: (process.env.REPORT_GAS) ? true : false
+  },
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: "PSW8C433Q667DVEX5BCRMGNAH9FSGFZ7Q8"
   },
   typechain: {
     outDir: 'typechain',
