3496f0e1b2197eb58ebf7c4dcef19f163e1cc361
adairrr <adair.kelley@vanderbilt.edu>
2021-05-01 19:40:27 +0000

    Add a prometheus exporter plugin to the textile server

--- packages/textile-server/src/index.ts
+++ packages/textile-server/src/index.ts
@@ -1,25 +1,44 @@
 import { ResolverContext } from './graphql/resolverContext';
 import resolvers from './graphql/resolvers';
-import { ApolloServer } from 'apollo-server';
+import { ApolloServer } from 'apollo-server-express';
 import { schema } from './graphql/schemaLoader';
+import express from 'express';
 
 import * as dotenv from "dotenv";
 import Textile from './textile/textile';
+import { createPrometheusExporterPlugin } from '@bmatei/apollo-prometheus-exporter';
 
 dotenv.config()
 
 Textile.Instance().then(async (t) => {
+
+  const app = express();
+
+  console.log("Creating the Prometheus exporter plugin")
+  const prometheusExporterPlugin = createPrometheusExporterPlugin({ app });
+
   const server = new ApolloServer({
     typeDefs: schema,
     resolvers: resolvers,
     introspection: true,
     playground: true,
+    plugins: [prometheusExporterPlugin],
     context: new ResolverContext(t),
     tracing: true
   });
 
-  server.listen({ port: process.env.TEXTILE_SERVER_PORT }).then(({ url, subscriptionsUrl }) => {
-    console.log(`🚀 Server ready at ${url}`)
-    console.log(`🚀 Server ready at ${subscriptionsUrl}`)
-  });
+  console.log("Applying middleware")
+
+  server.applyMiddleware({ app, path: '/' });
+
+  console.log("Attempting to start the apollo server")
+  try {
+    app.listen({ port: process.env.TEXTILE_SERVER_PORT }, () => {
+      console.log(`🚀 Server ready at http://localhost:${process.env.TEXTILE_SERVER_PORT}`)
+      // console.log(`🚀 Server ready at ${subscriptionsUrl}`)
+    })
+  } catch (err) {
+    console.log(err);
+  }
+
 }).catch(error => console.log(error));
